263b73f4e4d07349b6c0b202ab0a4cb97447fb51
ReiWax <50460270+ReiWax@users.noreply.github.com>
2022-06-09 12:00:47 +0200

    First Commit

---
+++ pizza-back/src/main/java/fr/eni/PizzaBackApplication.java
@@ -0,0 +1,13 @@
+package fr.eni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaBackApplication.class, args);
+	}
+
+}
